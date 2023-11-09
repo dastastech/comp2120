75f90ec8bbc6a1099d8ef58e29a4c0922c079961
dastastech <dastastech@gmail.com>
2023-11-09 05:21:41 -0800

    Update labs.

--- m04/arreglos/arreglo-ciclo01.py
+++ m04/arreglos/arreglo-ciclo01.py
@@ -5,3 +5,7 @@ for i in range(len(colores)):
     # print(colores[i])
     if (colores[i] == 'azul'):
         print (f'{colores[i]} es mi color favorito ')
+
+for color in colores:
+    if (color == 'azul'):
+        print (f'{color} es mi color favorito ')
