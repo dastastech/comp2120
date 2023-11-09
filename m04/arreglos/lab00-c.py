c7a9bc1dfe5dba577da03891948d226a2a8acca6
dastastech <dastastech@gmail.com>
2023-11-09 05:23:28 -0800

    Update labs.

--- m04/arreglos/lab00-c.py
+++ m04/arreglos/lab00-c.py
@@ -1,7 +1,9 @@
 myFavoritePlaces = ['Roma','Hatillo','New York', 'Camuy']
 
 print(myFavoritePlaces)
+
 indice = int( input('Indique la posición a cambiar: ')  )
+
 print(myFavoritePlaces[indice])
 myFavoritePlaces[indice] = "Kiev"
 print(myFavoritePlaces[indice])
